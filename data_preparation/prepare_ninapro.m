89b98559d650100cf2e1625a1c5872fe5b8e2de9
sameeksha-ramesh <sameeksharamesh2005@gmail.com>
2026-03-29 23:44:24 +0530

    Update prepare_ninapro.m

--- data_preparation/prepare_ninapro.m
+++ data_preparation/prepare_ninapro.m
@@ -1,1 +1,35 @@
+clc; clear;
 
+% Load NinaPro dataset file
+load('S1_E1_A1.mat');
+
+emg_data = emg;
+labels = restimulus;
+
+% Remove rest class (0)
+valid_idx = labels ~= 0;
+emg_data = emg_data(valid_idx,:);
+labels = labels(valid_idx);
+
+window_size = 200;
+step_size = 200;
+
+segments = [];
+segment_labels = [];
+
+for i = 1:step_size:(length(emg_data)-window_size)
+
+    window = emg_data(i:i+window_size-1,:);
+    label_window = labels(i:i+window_size-1);
+
+    % Majority label in window
+    label = mode(label_window);
+
+    segments = cat(3, segments, window');
+    segment_labels = [segment_labels; label];
+
+end
+
+save('emg_segments.mat','segments','segment_labels');
+
+disp("Saved emg_segments.mat");
