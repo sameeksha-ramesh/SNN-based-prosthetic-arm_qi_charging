23c51b70411d4b7205d1a65bdb522d43686333d1
sameeksha-ramesh <sameeksharamesh2005@gmail.com>
2026-03-29 23:56:45 +0530

    Update export_weights.py

--- SNN_Training/export_weights.py
+++ SNN_Training/export_weights.py
@@ -1,1 +1,13 @@
+import torch
+import numpy as np
+from snn_model import SNN
 
+spikes = torch.load("data/spikes.pt")
+
+model = SNN(spikes.shape[2])
+model.load_state_dict(torch.load("snn_model.pth"))
+
+np.save("fc1_w.npy", model.fc1.weight.detach().numpy())
+np.save("fc2_w.npy", model.fc2.weight.detach().numpy())
+
+print("Weights Exported")
